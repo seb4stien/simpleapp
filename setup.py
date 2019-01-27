62e8d465970eb415001c5288031c20c8b9cafdbb
Sébastien Georget <sebastien.georget@orange.com>
2019-01-27 18:59:00 +0100

    Minimal build

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="simpleapp",
+    version="0.0.1",
+    author="seb4stien",
+    author_email="seb4stien@georget.name",
+    description="Simple app",
+)
