3ccacd08c1420f87ac69b40c75c0477b786fad1b
Sebastien Georget <sebastien.georget@orange.com>
2019-01-30 09:33:41 +0100

    Basic front

--- simpleapp/front.py
+++ simpleapp/front.py
@@ -1,11 +1,30 @@
+"""
+Trivial frontend
+"""
+
+# stdlib
+import socket
+
+# dependencies
 from flask import Flask
 
-app = Flask(__name__)
+# local
+from simpleapp.common import Backend
 
+
+myname = socket.gethostname()
+
+app = Flask(__name__)
+app.config["MYNAME"] = myname
+
+backend = Backend()
+backend.register(myname)
 
 @app.route('/')
 def index():
-    return "Index"
+    return "Hello, I am {} in a group of {}".format(
+            app.config['MYNAME'],
+            backend.members())
 
-
-app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=1235)
