29b8ddc9f97f587b97693d9cf0a24cb28d639838
Sebastien Georget <sebastien.georget@orange.com>
2019-01-30 14:26:54 +0100

    Cleaning

--- simpleapp/worker.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import socket
-import time
-
-while True:
-    print("[{}] Running on {}".format(
-          datetime.now().strftime("%H:%M:%S"),
-          socket.gethostname()))
-    time.sleep(10)
