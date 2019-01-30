0781c787f5a85383c886a97c437ab3a982b849b8
Sebastien Georget <sebastien.georget@orange.com>
2019-01-30 15:40:56 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ setuptools.setup(
     author="seb4stien",
     author_email="seb4stien@georget.name",
     description="Simple app",
+    packages=['simpleapp'],
+    python_requires='>3.4.3',
+    install_requires=['Flask', 'redis', 'requests']
 )
